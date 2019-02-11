d67f8e5c8b1d727a08a8d80994b71e56dcc518b6
Geometer1729 <kuhnsbt@clarkson.edu>
2019-02-11 09:08:28 -0500

    refactor C to be less redundant

--- c/exec.c
+++ c/exec.c
@@ -7,73 +7,36 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-// I realize this code is ugly
-
 int* cexec(char* filePath,void** newargv,void** env,int pipeO,int pipeE){
   newargv[0] = filePath;
-  if(pipeO){
-    if(pipeE){ // pipe both
-      int pipeOfd[2]; 
-      pipe(pipeOfd);
-      int  pipeEfd[2]; 
-      pipe(pipeEfd);
-      int pid = fork();
-      if (pid == 0){ //child
-        close(pipeOfd[0]);
-        dup2( pipeOfd[1],1);
-        close(pipeEfd[0]);
-        dup2( pipeEfd[1],2);
-        execve(filePath,(char**)newargv,(char**)env);
-      }else{ //parent
-        close(pipeOfd[1]);
-        close(pipeEfd[1]);
-        int* ret = (int*)malloc(sizeof(int)*3);
-        ret[0] = pid;
-        ret[1] = pipeOfd[0];
-        ret[2] = pipeEfd[0];
-        return ret;
-      }
-    }else{
-      int pipeOfd[2]; 
-      pipe(pipeOfd);
-      int pid = fork();
-      if (pid == 0){ //child
-        close(pipeOfd[0]);
-        dup2( pipeOfd[1],1);
-        execve(filePath,(char**)newargv,(char**)env);
-      }else{ //parent
-        close(pipeOfd[1]);
-        int* ret = (int*)malloc(sizeof(int)*2);
-        ret[0] = pid;
-        ret[1] = pipeOfd[0];
-        return ret;
-      }
+  int pipeOfd[2]; 
+  int  pipeEfd[2]; 
+  if(pipeO)
+    pipe(pipeOfd);
+  if(pipeE)
+    pipe(pipeEfd);
+  int pid = fork();
+  if(pid == 0){ //child
+    if(pipeO){
+      close(pipeOfd[0]);
+      dup2( pipeOfd[1],1);
     }
-  }else{
     if(pipeE){
-      int  pipeEfd[2]; 
-      pipe(pipeEfd);
-      int pid = fork();
-      if (pid == 0){ //child
-        close(pipeEfd[0]);
-        dup2( pipeEfd[1],2);
-        execve(filePath,(char**)newargv,(char**)env);
-      }else{ //parent
-        close(pipeEfd[1]);
-        int* ret = (int*)malloc(sizeof(int)*2);
-        ret[0] = pid;
-        ret[1] = pipeEfd[0];
-        return ret;
-      }
-    }else{
-      int pid = fork();
-      if (pid == 0){ //child
-        execve(filePath,(char**)newargv,(char**)env);
-      }else{ //parent
-        int* ret = (int*)malloc(sizeof(int)*1);
-        ret[0] = pid;
-        return ret;
-      }
+      close(pipeEfd[0]);
+      dup2( pipeEfd[1],2);
     }
+    execve(filePath,(char**)newargv,(char**)env);
+  }else{ //parent
+    if(pipeO)
+      close(pipeOfd[1]);
+    if(pipeE)
+      close(pipeEfd[1]);
+    int* ret = (int*)malloc(sizeof(int)*(1+pipeO+pipeE));
+    ret[0] = pid;
+    if(pipeO)
+      ret[1] = pipeOfd[0];
+    if(pipeE)
+      ret[1+pipeO] = pipeEfd[0];
+    return ret;
   }
 }
