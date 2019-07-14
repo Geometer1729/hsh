283c0c593ed93073cf5ba67373b5fedbe5ff9ec9
Geometer1729 <16kuhnb@gmail.com>
2019-07-14 00:59:28 -0400

    Finish basic parser

--- parse.hs
+++ parse.hs
@@ -13,7 +13,13 @@ parseCommand s = let parser = readP_to_S cmdParser
                     then fst . head $ finished
                     else error $ show parses
 cmdParser :: ReadP Command
-cmdParser = parseExtract <++ parseExec 
+cmdParser = parseExtract <++ parseSubExtract
+
+parseSubExtract :: ReadP Command
+parseSubExtract = parsePipe <++ parseSubPipe
+
+parseSubPipe :: ReadP Command
+parseSubPipe = parseExec
 
 parseExec :: ReadP Command
 parseExec = do
@@ -27,11 +33,17 @@ parseWord = munch (/= ' ')
 parseArgs :: ReadP [String]
 parseArgs = many (char ' ' >> parseWord)
 
-
 parseExtract :: ReadP Command
 parseExtract = do
   var <- parseWord
   string " <- "
-  cmd <- cmdParser
+  cmd <- parseSubExtract
   return $ Extract var cmd
+
+parsePipe :: ReadP Command
+parsePipe = do
+  cmd1 <- parseSubPipe
+  string " >>= "
+  cmd2 <- parseSubExtract
+  return $ Pipe cmd1 cmd2
 
